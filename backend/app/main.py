4f060bd4072cdc602daa85c0478790ae63b4564f
Timo Engler <timo.engler1@web.de>
2025-03-07 17:53:41 +0100

    initial dockerfile frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI is running!"}
+
+
+def get_png_file():
+    """ Lädt ein gespeichertes PNG-Bild aus dem Ordner 'images/' """
+    file_path = os.path.join("images", "diagramm.png")
+
+    # Überprüfen, ob die Datei existiert
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Datei nicht gefunden: {file_path}")
+
+    return file_path  # Gibt den Dateipfad zurück
