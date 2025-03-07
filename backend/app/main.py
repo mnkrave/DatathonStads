b7526a8bfb162a3357c6273f8ade8e0f764268e0
Timo Engler <timo.engler1@web.de>
2025-03-07 20:01:28 +0100

    initial dockerfile frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI is running!"}
+
+@app.get("/image")
+def get_image():
+    """API-Endpunkt, um das PNG-Bild abzurufen"""
+    return get_png_file()
